394e4b0465b24337fc3ab49a2f6836a6b2c1d391
firedream89 <briand-kevin@hotmail.fr>
2020-11-22 18:03:13 +0100

    Update system.update().lua

--- Machine_Monitoring/system.update().lua
+++ Machine_Monitoring/system.update().lua
@@ -11,20 +11,25 @@ function getStatusName(status)
     end
 end
 
+local data = {
+  name = {},
+  state = {}
+}
+
 local style = "<style>th { font-size: "..fontSize.."em; text-align: left; vertical-align: middle; border-bottom: 3px solid #ddd; }</style>"
 local html = style.."<p style='font-size: "..fontSize.."em; text-align: center; margin-top: 15px; margin-bottom: 15px;'>"..tableName.."</p><table width=100%>"
 
-for i = 1, #machines
-do
+for i = 1, #machines do
 	html = html.."<tr><th>"..slotsName[i].."</th><th>"..getStatusName(machines[i].getStatus()).."</th></tr>"
+	
+     data.name[#data.name+1] = slotsName[i]
+     data.state[#data.state+1] = getStatusName(machines[i].getStatus())
 end
 
 html = html.."</table>"
- 
+
+SaveData(unit.getId(),data)
+
 if Screen then
 	Screen.setHTML(html)
-    end
-
-if Emitter then
-    Emitter.send("test", "message")
     end
